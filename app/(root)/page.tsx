d55c8f9ad9bfd40d0b0e53552238318a61c9063c
David <montanezdavid@gmail.com>
2025-02-25 17:35:55 -0600

    Installed shadcn ui library component. Add: Search bar feature and URL parameter function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({ searchParams}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="px-5">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Picthes, and Get Noticed in Virtual Competitions
+        </p>
+
+        <SearchForm
+          query={query}
+        />
+      </section>
     </>
   );
 }
